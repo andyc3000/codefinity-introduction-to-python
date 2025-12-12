8640cacaa300f591bfcb7590863c5e28e8c57231
andyc3000 <>
2025-12-12 20:15:26 +0000

    Challenge: Pricing Adjustment Capstone

---
+++ other_data_types/challenge_pricing_adjustment_capstone/main.py
@@ -0,0 +1,5 @@
+grocery_inventory = {"Milk": ("Dairy", 3.50, 8), "Eggs": ("Dairy", 5.50, 30), "Bread": ("Bakery", 2.99, 15), "Apples": ("Produce", 1.50, 50)}
+
+eggs_price = grocery_inventory["Eggs"][1]
+
+print(eggs_price)
