cb48fac77784f2d7eaa0e45999cdb28c80116e2e
andyc3000 <>
2026-01-02 10:45:30 +0000

    User-defined functions

--- functions/user_defined_functions/main.py
+++ functions/user_defined_functions/main.py
@@ -5,5 +5,5 @@ def calculate_total_cost(price, quantity):
 
 apples_total_cost = calculate_total_cost(1.50, 10)
 
-print(apples_total_cost)
+print("The total cost for apples is $",apples_total_cost)
 
